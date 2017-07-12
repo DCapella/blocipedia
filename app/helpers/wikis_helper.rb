71dabf4020c23968d2b6a39a9225004ecd1c75b3
David Capella <D_Capella@yahoo.com>
2017-07-11 23:23:15 -0500

    edited if a wiki is even private and updated wiki_helper to hold true or false

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -2,4 +2,8 @@ module WikisHelper
   def user_is_authorized_for_wiki?(wiki)
     current_user && (current_user == wiki.user || current_user.admin?)
   end
+
+  def user_is_authorized_for_private_wiki?(wiki)
+    current_user.admin? || current_user.premium? || !wiki.private
+  end
 end
